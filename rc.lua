ce08a554a8f3e9f87e5d545244dfb4828f4b043a
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2017-03-21 19:25:44 -0400

    eclipse a la pantalla 3

--- rc.lua
+++ rc.lua
@@ -508,6 +508,8 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
      { rule = { class = "Firefox" },
        properties = { screen = 1, tag = "2" } },
+     { rule = { class = "Eclipse" },
+       properties = { screen = 1, tag = "3" } },
 }
 -- }}}
 
